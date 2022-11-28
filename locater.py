19b4a1e76fbd29c1cbc9c5cdd4b2d4eb0f2d3b48
liam-ely <liam.ely@farmersedge.ca>
2022-11-28 14:01:32 -0600

    updated question

--- locater.py
+++ locater.py
@@ -1,7 +1,5 @@
 """
 Write a function that takes latitude and longitude and returns the street address, if present.
-Make it as resilient as you can, within reason.
-
 Use the Requests module and the Nominatim API, documentation found here:
     https://nominatim.org/release-docs/latest/api
 """
