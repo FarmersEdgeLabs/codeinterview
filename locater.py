e3dcda931be9deca47ab21847990731514a7226d
liam-ely <liam.ely@farmersedge.ca>
2022-11-28 12:56:27 -0600

    changed question

---
+++ locater.py
@@ -0,0 +1,10 @@
+"""
+Write a function that takes latitude and longitude and returns the country where the coordinates are located.
+
+Use the Requests library and the Nominatim API, documentation found here:
+    https://nominatim.org/release-docs/latest/api
+"""
+
+
+def locate():
+    return
