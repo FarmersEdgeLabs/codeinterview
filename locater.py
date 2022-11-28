35707fb7f5eac4fbe01541adb3a084ae39244ab9
liam-ely <liam.ely@farmersedge.ca>
2022-11-28 13:46:02 -0600

    updated question

--- locater.py
+++ locater.py
@@ -1,7 +1,8 @@
 """
-Write a function that takes latitude and longitude and returns the country where the coordinates are located.
+Write a function that takes latitude and longitude and returns the street address, if present.
+Make it as resilient as you can, within reason.
 
-Use the Requests library and the Nominatim API, documentation found here:
+Use the Requests module and the Nominatim API, documentation found here:
     https://nominatim.org/release-docs/latest/api
 """
 
