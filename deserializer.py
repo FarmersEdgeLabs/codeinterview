b320482a02c69e3dfa0f1d97ade1da53579c0145
liam-ely <102809975+liam-ely@users.noreply.github.com>
2022-12-05 15:06:10 -0600

    Update deserializer.py

--- deserializer.py
+++ deserializer.py
@@ -1,5 +1,5 @@
 """
-Detail an Order class that initializes with a string in JSON format, with attributes described in the string.
+Write an object with an initializer that takes in the given json_input string and builds the object with attributes and values described by the input string.
 """
 json_input = '{"order_id":01745,"customerid":305,"cost":40.76,"dateordered":"10-14-2021","datecompleted":NULL}'
 
